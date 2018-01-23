1dc935a00894cf8068c970207471c93f1c295a3b
sam-almighty <sam.almighty85@gmail.com>
2018-01-23 14:37:19 +0530

    Refactored region end points and updated payloads (#25)
    * Updated SDK to include BACKGROUND_REFRESH policy for actions
    
    * Updated ReadMe for BACKGROUND_REFRESH
    
    * Add Destroy method
    
    * Included methods to clear cache
    
    * Updated url end point
    
    * Updated regions with correct values
    
    * Refactored
    
    * Updated read

--- AppLaunchClientAndroid/applaunch-client-android/src/main/java/com/ibm/mobile/applaunch/android/api/ICRegion.java
+++ AppLaunchClientAndroid/applaunch-client-android/src/main/java/com/ibm/mobile/applaunch/android/api/ICRegion.java
@@ -7,23 +7,24 @@ package com.ibm.mobile.applaunch.android.api;
 public enum ICRegion {
 
 
-    US_SOUTH(".us-south.containers.mybluemix.net"),
+    US_SOUTH(".ng.bluemix.net"),
 
-    UNITED_KINGDOM(".eu-gb.containers.mybluemix.net"),
+    UNITED_KINGDOM(".eu-gb.bluemix.net"),
 
-    SYDNEY(".sydney.containers.mybluemix.net"),
+    SYDNEY(".au-syd.bluemix.net"),
 
-    US_SOUTH_STAGING("-staging.us-south.containers.mybluemix.net"),
+    US_SOUTH_STAGING(".stage1.ng.bluemix.net"),
 
-    UNITED_KINGDOM_STAGING("-staging.eu-gb.containers.mybluemix.net"),
+    UNITED_KINGDOM_STAGING(".stage1.eu-gb.bluemix.net"),
 
-    SYDNEY_STAGING("-staging.sydney.containers.mybluemix.net"),
+    SYDNEY_STAGING(".stage1.au-syd.mybluemix.net"),
 
-    US_SOUTH_DEV("-dev.us-south.containers.mybluemix.net"),
+    US_SOUTH_DEV(".dev.ng.mybluemix.net"),
 
-    UNITED_KINGDOM_DEV("-dev.eu-gb.containers.mybluemix.net"),
+    UNITED_KINGDOM_DEV(".dev.eu-gb.bluemix.net"),
 
-    SYDNEY_DEV("-dev.sydney.containers.mybluemix.net");
+    SYDNEY_DEV(".dev.au-syd.bluemix.net");
+
 
     private String region;
 
--- README.md
+++ README.md
@@ -74,7 +74,7 @@ To use the Bluemix App Launch SDK include the following:
     ```
     dependencies {
         ........
-        compile 'com.github.ibm-bluemix-mobile-services:bms-clientsdk-android-applaunch:0.0.1'
+        compile 'com.github.ibm-bluemix-mobile-services:bms-clientsdk-android-applaunch:0.0.5'
         compile 'com.ibm.mobilefirstplatform.clientsdk.android:core:[2.0.0,3.0.0)'
         .......
     }
