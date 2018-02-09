14c65bfdd2518953974f02f564922921453762c7
Vittal Pai <vittalpai@ymail.com>
2018-02-09 14:28:16 +0530

    Revert "Merge master"
    This reverts commit c9f6cc4aee3b41a495b774ce4fef4d21499b4806, reversing
    changes made to 4347ad9dc4e962419732ad6978a932477178929e.

--- AppLaunchClientAndroid/applaunch-client-android/src/main/java/com/ibm/mobile/applaunch/android/api/AppLaunchListener.java
+++ AppLaunchClientAndroid/applaunch-client-android/src/main/java/com/ibm/mobile/applaunch/android/api/AppLaunchListener.java
@@ -10,11 +10,6 @@
  *     See the License for the specific language governing permissions and
  *     limitations under the License.
  */
-<<<<<<< HEAD
-=======
-
-package com.ibm.mobile.applaunch.android.api;
->>>>>>> master
 
 package com.ibm.mobile.applaunch.android.api;
 
