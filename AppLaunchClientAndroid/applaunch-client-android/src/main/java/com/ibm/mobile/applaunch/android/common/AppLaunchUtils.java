a25ab07149177ab4fb788c3e2dd9741b2fb16431
Vittal Pai <vittalpai@ymail.com>
2018-02-20 04:26:51 +0530

    Change Platform Name
     bluemix-mobile-services/bmd-project-applauch#627

--- AppLaunchClientAndroid/applaunch-client-android/src/main/java/com/ibm/mobile/applaunch/android/common/AppLaunchUtils.java
+++ AppLaunchClientAndroid/applaunch-client-android/src/main/java/com/ibm/mobile/applaunch/android/common/AppLaunchUtils.java
@@ -85,19 +85,13 @@ public class AppLaunchUtils {
 
     private static String getPlatform() {
         // return Locale.getDefault().getLanguage();
-        return "android";
+        return "G";
     }
 
     public static JSONObject getInitJson(Application application) {
         JSONObject initObject = new JSONObject();
         try {
-          //  initObject.put("model", getModel());
-          //  initObject.put("brand", getBrand());
-         //   initObject.put("OSVersion", getOSVersion());
             initObject.put("platform", getPlatform());
-          //  initObject.put("appId", getPackageName(application));
-         //   initObject.put("appVersion", "1.0.0");
-         //   initObject.put("appName", getAppName(application));
         } catch (JSONException e) {
             initObject = null;
             logger.error("Error creating init json " + e.getMessage());
